a6e8b5ed5e9d7f4c71439559486c7c9144d6266d
Xavier <xbrochard@pc13381.idsi0.si.c-s.fr>
2016-06-15 14:36:07 +0200

    Completed lesson 27

--- Egghead/main.js
+++ Egghead/main.js
@@ -4,6 +4,7 @@ import deepFreeze from 'deep-freeze'
 import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
+import { connect } from 'react-redux'
 
 // Reducers
 const todo = (state, action) => {
@@ -341,45 +342,36 @@ const getVisibleTodos = ( todos, filter) => {
   }
 }
 
-class VisibleTodoList extends React.Component {
-  componentDidMount() {
-    const { store } = this.context;
-    this.unsubscribe = store.subscribe(() => this.forceUpdate());
-  }
-
-  componentWillUnmount() {
-    this.unsubscribe();
-  }
-
-  render () {
-    const props = this.props
-    const { store } = this.context
-    const state = store.getState()
-
-    return (
-      <TodoList
-        todos={getVisibleTodos(state.todos, state.visibilityFilter)}
-        onTodoClick={id => store.dispatch({
-          type: 'TOGGLE_TODO',
-          id
-        })}
-      />
-    )
+const mapStateToProps = (state) => {
+  return {
+    todos: getVisibleTodos(state.todos, state.visibilityFilter)
   }
 }
-VisibleTodoList.contextTypes = {
-  store: React.PropTypes.object
+
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTodoClick: (id) =>
+      dispatch({
+        type: 'TOGGLE_TODO',
+        id
+      })
+  }
 }
+
+const VisibleTodoList = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(TodoList)
 
 let nexTodoId = 0;
 
 const TodoApp = () => (
-    <div>
-      <AddTodo />
-      <VisibleTodoList />
-      <Footer />
-    </div>
-  )
+  <div>
+    <AddTodo />
+    <VisibleTodoList />
+    <Footer />
+  </div>
+)
 
 ReactDOM.render(
   <Provider store={createStore(todoApp)}>
